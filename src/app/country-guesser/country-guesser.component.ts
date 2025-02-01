2c4a73681dec8ce8c276524a0383628d8f9cd6ea
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-02-01 19:14:22 +0100

    set image as not draggable

--- src/app/country-guesser/country-guesser.component.ts
+++ src/app/country-guesser/country-guesser.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { worldCountries } from './data/worldCountries';
 import { similarCountryFlags } from './data/similarCountryFlags';
