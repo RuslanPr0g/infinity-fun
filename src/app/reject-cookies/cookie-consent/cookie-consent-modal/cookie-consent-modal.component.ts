60ad904ab945c98bb15caec41b16cf9226caded0
RuslanPr0g <theruslan.prog@gmail.com>
2025-06-19 13:43:00 +0200

    add

--- src/app/reject-cookies/cookie-consent/cookie-consent-modal/cookie-consent-modal.component.ts
+++ src/app/reject-cookies/cookie-consent/cookie-consent-modal/cookie-consent-modal.component.ts
@@ -12,32 +12,34 @@ import { CookieConsentService } from '../cookie-consent.service';
 export class CookieConsentModalComponent {
   @Output() closed = new EventEmitter<void>();
 
-  showPreferences = true;//false;
+  showPreferences = false;
 
   absurdOptions = [
     'Yes, inject uranium directly into my bloodstream',
     'Absolutely, wire my thoughts to a nuclear reactor',
-    'Sure, replace my bones with vibrating tungsten rods',
-    'Yes, beam encrypted lava pulses into my spine',
-    'Go ahead, calibrate my emotions using plutonium dust',
-    'Definitely, sync my heartbeat to the Hadron Collider',
-    'Yes, initiate the titanium mandible replacement protocol',
-    'Proceed with algorithmic volcano core integration',
-    'Affirmative, install a graphite-cooled brain enhancer',
-    'Enable full-body cryogenic self-destruction response',
-    'Authorize transdimensional fax transmission of my soul',
-    'Yes, ignite my memory center with industrial-grade jet fuel',
-    'Begin neural interface with sentient microwave ovens',
-    'Consent granted for perpetual entropy realignment',
-    'Yes, replace all teeth with miniature fusion reactors',
-    'Upload my conscience into the municipal sewage grid',
-    'Initiate orbital laser mood stabilization sequence',
-    'Substitute my blood with caffeinated hydraulic fluid',
-    'Install quantum banana peels in my motor cortex',
-    'Affix anti-matter dampeners to my self-esteem module',
+    // 'Sure, replace my bones with vibrating tungsten rods',
+    // 'Yes, beam encrypted lava pulses into my spine',
+    // 'Go ahead, calibrate my emotions using plutonium dust',
+    // 'Definitely, sync my heartbeat to the Hadron Collider',
+    // 'Yes, initiate the titanium mandible replacement protocol',
+    // 'Proceed with algorithmic volcano core integration',
+    // 'Affirmative, install a graphite-cooled brain enhancer',
+    // 'Enable full-body cryogenic self-destruction response',
+    // 'Authorize transdimensional fax transmission of my soul',
+    // 'Yes, ignite my memory center with industrial-grade jet fuel',
+    // 'Begin neural interface with sentient microwave ovens',
+    // 'Consent granted for perpetual entropy realignment',
+    // 'Yes, replace all teeth with miniature fusion reactors',
+    // 'Upload my conscience into the municipal sewage grid',
+    // 'Initiate orbital laser mood stabilization sequence',
+    // 'Substitute my blood with caffeinated hydraulic fluid',
+    // 'Install quantum banana peels in my motor cortex',
+    // 'Affix anti-matter dampeners to my self-esteem module',
   ];
 
   selectedOptions = new Set(this.absurdOptions);
+
+  allSelected: boolean = false;
 
   constructor(private consentService: CookieConsentService) { }
 
@@ -63,7 +65,12 @@ export class CookieConsentModalComponent {
     } else {
       this.selectedOptions.add(option);
     }
-    this.trollBehavior();
+
+    if (this.selectedOptions.size === 0) {
+      this.allSelected = true;
+    } else {
+      this.trollBehavior();
+    }
   }
 
   private trollBehavior() {
