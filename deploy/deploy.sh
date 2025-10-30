30b3663516843372a2b0b5db9071b3b6ed6767d9
RuslanPr0g <theruslan.prog@gmail.com>
2025-10-30 09:41:41 +0100

    updates

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,18 +2,6 @@
 
 echo "Starting Angular deployment..."
 
-echo "Minifying group JSON files..."
-
-# Run the node minify-json script
-node minify-json.js
-if [ $? -ne 0 ]; then
-    echo "❌ JSON minification failed with exit code $?."
-    exit $?
-fi
-
-echo "✅ JSON minification completed."
-
-# Run Angular deploy with base href
 ng deploy --base-href=/infinity-fun/
 if [ $? -ne 0 ]; then
     echo "❌ Deployment failed with exit code $?."
